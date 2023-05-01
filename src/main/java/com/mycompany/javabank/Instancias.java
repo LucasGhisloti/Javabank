21218022d6ea54202e97cc1341c1f234df589448
LucasGhisloti <l.ghisloti@unesp.br>
2023-04-30 21:15:36 -0300

    Instancias: usa Models para Banco
    Co-authored-by: Guilherme H. Jardim <49923210+ghjardim@users.noreply.github.com>

--- src/main/java/com/mycompany/javabank/Instancias.java
+++ src/main/java/com/mycompany/javabank/Instancias.java
@@ -11,17 +11,24 @@ package com.mycompany.javabank;
 public class Instancias {
     
     Instancias(){
-        Banco b1 = new Banco(1, "Banco Atlas");
-        Banco b2 = new Banco(2, "Fênix Financeiro");
-        Banco b3 = new Banco(3, "Prestige Investimentos");
+        Models model = new Models();
+
+        model.listaBanco.add(new Banco(1, "JavaBank"));
+        model.listaBanco.add(new Banco(2, "Banco Atlas"));
+        model.listaBanco.add(new Banco(3, "Fenix Financeiro"));
+        model.listaBanco.add(new Banco(4, "Prestige Investimentos"));
 
         Cliente cli = new Cliente(1, "Arnaldo Sacomani", "54.986.782-89", 9632, 78963214);
         Conta conta1 = new Conta(1, 1, "conta corrente", 30000.00, 2000.00);
         Transacao trans = new Transacao(0, "2023-02-01", "08:11:23", 1, 1, 200.30, "Saque");
 
-        System.out.println(trans.toString());
+        for(int i = 0; i< model.listaBanco.size(); i++){
+            System.out.println(model.listaBanco.get(i).getNome());
+        }
+
+        /*System.out.println(trans.toString());
         
-        /*System.out.println(cli.getID() + " " + cli.getNome());
+        System.out.println(cli.getID() + " " + cli.getNome());
         System.out.println(conta1.getSaldo() + " " + conta1.getLimiteSaque());
         if (cli.Sacar(conta1, 2000.01)){
             System.out.println(conta1.getSaldo());
