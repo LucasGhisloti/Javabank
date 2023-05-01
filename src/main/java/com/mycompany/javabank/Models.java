69cdd0030d026e9088086b171dcfef9cf9997c20
LucasGhisloti <l.ghisloti@unesp.br>
2023-04-30 21:11:45 -0300

    Adiciona Models
    Co-authored-by: Guilherme H. Jardim <49923210+ghjardim@users.noreply.github.com>

---
+++ src/main/java/com/mycompany/javabank/Models.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javabank;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lucas Ghisloti
+ */
+public class Models {
+    ArrayList<Cliente> listaCliente = new ArrayList<>();
+    ArrayList<Conta> listaConta = new ArrayList<>();
+    ArrayList<Transacao> listaTransacao = new ArrayList<>();
+    ArrayList<Banco> listaBanco = new ArrayList<>();
+    
+}
