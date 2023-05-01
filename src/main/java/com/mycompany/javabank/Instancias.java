631245c2525fea2ca49e535cd4cea4e63916f2fd
LucasGhisloti <l.ghisloti@unesp.br>
2023-04-30 21:29:47 -0300

    Instancias: usa Models para Cliente
    Co-authored-by: Guilherme H. Jardim <49923210+ghjardim@users.noreply.github.com>

--- src/main/java/com/mycompany/javabank/Instancias.java
+++ src/main/java/com/mycompany/javabank/Instancias.java
@@ -18,12 +18,18 @@ public class Instancias {
         model.listaBanco.add(new Banco(3, "Fenix Financeiro"));
         model.listaBanco.add(new Banco(4, "Prestige Investimentos"));
 
-        Cliente cli = new Cliente(1, "Arnaldo Sacomani", "54.986.782-89", 9632, 78963214);
         Conta conta1 = new Conta(1, 1, "conta corrente", 30000.00, 2000.00);
         Transacao trans = new Transacao(0, "2023-02-01", "08:11:23", 1, 1, 200.30, "Saque");
 
-        for(int i = 0; i< model.listaBanco.size(); i++){
-            System.out.println(model.listaBanco.get(i).getNome());
+        model.listaCliente.add(new Cliente(1, "Arnaldo Sacomani", "54.986.782-89", 9632, 78963214));
+        model.listaCliente.add(new Cliente(2, "Jorge Ben", "98.741.951-27", 1234, 12345678));
+        model.listaCliente.add(new Cliente(3, "John McCarthy", "57.563.951-19", 5637, 78932165));
+        model.listaCliente.add(new Cliente(4, "Ada Lovelace", "85.325.859-32", 6295, 84951623));
+        model.listaCliente.add(new Cliente(5, "Alan Turing", "12.988.556-78", 7562, 26159483));
+        model.listaCliente.add(new Cliente(6, "Orlando de Andrade Figueiredo", "96.556.001-51", 4859, 32654898));
+
+        for(int i = 0; i< model.listaCliente.size(); i++){
+            System.out.println(model.listaCliente.get(i).getSenhaTransac());
         }
 
         /*System.out.println(trans.toString());
